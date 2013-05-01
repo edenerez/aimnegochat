aeacca6235fb5bfecc880e5de48b6bd0afcbbb7d
erelsgl <erelsgl@gmail.com>
2013-05-01 10:55:40 +0300

    fix translator and add grammar

--- translation.js
+++ translation.js
@@ -1,18 +1,23 @@
 // Translate text to semantic representation using a translation server:
-var HOST=process.env.TRANSLATION_SERVER_HOST || "localhost";
+var HOST=process.env.TRANSLATION_SERVER_HOST || "nlp-srv";
 var PORT=process.env.TRANSLATION_SERVER_PORT || 9994;
 console.log("Looking for translation server at "+HOST+":"+PORT);
 
-exports.Translator = function() {
+var fs=require('fs'), path=require('path');
+var DEFAULT_GRAMMAR_PATH = path.join(__dirname,"maps","NegotiationGrammarJson.txt");
+
+exports.Translator = function(translatorName, pathToGrammarFile) {
+	this.translatorName = translatorName;
+	this.grammar = fs.readFileSync(pathToGrammarFile? pathToGrammarFile: DEFAULT_GRAMMAR_PATH, 'utf8');
 	this.translationSocket = require('socket.io-client').connect(HOST, {port: PORT}); 
 	this.translationHandlers = [  /* initialize with a default handler */
 		function (text, translations) {
-			console.log("TranslationServer: "+translations.length+" translations to '"+text + "': "+JSON.stringify(translations));
+			console.log(translatorName + " receives "+translations.length+" translations to '"+text + "': "+JSON.stringify(translations));
 		}
 	];
-	
+
 	this.translationSocket.on('connect', function () { 
-		console.log("socket connected");
+		console.log(translatorName+" connected to translation server");
 		/* Test: */
 		//this.sendToTranslationServer("I offer a salary of 20000 and a car.", true, "NegotiationGrammarJson.txt");
 	});
@@ -21,6 +26,7 @@ exports.Translator = function() {
 	
 	var translator = this;
 	this.translationSocket.on('translation', function (result) {
+		//console.log(translatorName+" got a translation");
 		if (translator.textsWaitingForTranslations[result.text]) {
 			for (var i=0; i<translator.translationHandlers.length; ++i) 
 				translator.translationHandlers[i](result.text, result.translations);
@@ -30,14 +36,17 @@ exports.Translator = function() {
 }
 
 
-exports.Translator.prototype.sendToTranslationServer = function(text, forward, targetsFileName) {
-	console.log("TranslationClient: '" + text + "', '"+targetsFileName+"'");
+exports.Translator.prototype.sendToTranslationServer = function(text, forward) {
+	console.log(this.translatorName+" asks: '" + text + "'");
 	this.textsWaitingForTranslations[text] = true;
 	this.translationSocket.emit("translate", {
 		text: text,
 		forward: forward,
-		targetsFileName: targetsFileName,
-		numOfThreads: /*keep current number of threads*/ 0}
+		grammar: this.grammar,
+		numOfThreads: /*keep current number of threads*/ 0,
+		entailmentProbabilityThreshold: /* keep current threshold */ 0,
+		entailmentModel: /* keep current model */ undefined
+		}
 		);
 }
 
@@ -52,10 +61,12 @@ exports.Translator.prototype.onTranslation = function(translationHandler) {
 
 if (process.argv[1] === __filename) {
 	console.log("translation.js unitest start");
-	var translator1 = new exports.Translator();
-	var translator2 = new exports.Translator();
-	translator1.sendToTranslationServer("I offer a salary of 10000 and a car.", true, "NegotiationGrammarJson.txt");
-	translator2.sendToTranslationServer("I offer a salary of 20000 and a car.", true, "NegotiationGrammarJson.txt");
+	var translator1 = new exports.Translator("translator1");
+	var translator2 = new exports.Translator("translator2");
+	translator1.translationSocket.on('connect', function() {
+		translator1.sendToTranslationServer("I offer a salary of 10000 and a car.", true);
+	});
+	translator2.sendToTranslationServer("I offer a salary of 20000 and a car.", true);
 
 	// After several seconds, you should see only 2 results:
 	//   "TranslationServer: 2 translations to 'I offer a salary of 10000...
