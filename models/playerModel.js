804972f29169f79158e9389e84d936f178cf6f2b
osnatairy <osnatairy@gmail.com>
2013-05-07 13:57:28 +0300

    ReOrganize the database tables - part 2
    add a new database table that connect the game and player data.
    Reorder the view of the current tables.

---
+++ models/playerModel.js
@@ -0,0 +1,81 @@
+var azure = require('azure');
+ // , uuid = require('node-uuid');
+
+module.exports = PlayerModel;
+
+
+function PlayerModel(storageClient, tableName, partitionKey) {
+  this.storageClient = storageClient;
+  this.tableName = tableName;
+  this.partitionKey = partitionKey;
+
+
+  this.storageClient.createTableIfNotExists(tableName, 
+    function tableCreated(err) {
+      if(err) {
+        console.log("Cannot create table: "+JSON.stringify(err));
+        // throw err;
+      }
+    });
+};
+
+PlayerModel.prototype.add = function(item, callback) {
+	self = this;
+  //item.RowKey = uuid();
+  self.storageClient.insertEntity(self.tableName, item, 
+    function entityInserted(error) {
+      if(error){  
+        callback(error);
+      }
+      callback(null);
+    });
+};
+
+PlayerModel.prototype.deleteTable = function() {
+  self = this;
+  self.storageClient.deleteTable(self.tableName, function(error){
+    if(!error){
+        // Table deleted
+    }
+  });
+};
+
+PlayerModel.prototype.find = function(query, callback) {
+  self = this;
+  self.storageClient.queryEntities(query, 
+    function entitiesQueried(error, entities){
+      if(error) {
+        callback(error);
+      } else {
+        callback(null, entities);
+      }
+    });
+};
+
+
+PlayerModel.prototype.findOne = function(query, callback) {
+	self = this;
+  self.storageClient.queryEntity(query.tableName,
+                                 query.partitionKey,
+                                 query.rowKey, 
+    function entitiesQueried(error, entity){
+      if(error) {
+        callback(error);
+      } else {
+        callback(null, entity);
+      }
+    });
+};
+
+PlayerModel.prototype.updateItem = function(item, callback) {
+  self = this;
+  item.PartitionKey = self.partitionKey;
+    self.storageClient.insertOrMergeEntity (self.tableName, item, 
+    function entityInserted(error) {
+      if(error){  
+        callback(err);
+      }
+      callback(null);
+    });
+};
+ 
