327f016d98a4dcd46a325bcb849a7e9ea5e4b6a8
erelsgl <erelsgl@gmail.com>
2013-07-02 10:58:05 +0300

    change the translation unit

--- translation.js
+++ translation.js
@@ -1,56 +1,44 @@
-// Translate text to semantic representation using a translation server:
-var HOST=process.env.TRANSLATION_SERVER_HOST || "localhost";
+/**
+ * Translate text to semantic representation using a classifier-based translation server.
+ */
+
+var HOST=process.env.TRANSLATION_SERVER_HOST || "http://irsrv2.cs.biu.ac.il";
 var SETTINGS = {
-	port: process.env.TRANSLATION_SERVER_PORT || 9994, 
+	port: process.env.TRANSLATION_SERVER_PORT || 9995, 
 	'force new connection': true, 
 	'sync disconnect on unload': true
 };
-
-var fs=require('fs'), path=require('path');
-var DEFAULT_GRAMMAR_PATH = path.join(__dirname,"maps","NegotiationGrammarJson.txt");
 
 function logWithTimestamp(message) {
 	console.log(new Date().toISOString()+" "+message);
 }
 
-exports.Translator = function(translatorName, pathToGrammarFile) {
+exports.Translator = function(translatorName) {
 	logWithTimestamp(translatorName+" tries to connect to translation server at "+HOST+":"+SETTINGS.port);
 	this.translatorName = translatorName;
-	this.grammar = fs.readFileSync(pathToGrammarFile? pathToGrammarFile: DEFAULT_GRAMMAR_PATH, 'utf8');
 	this.translationSocket = require('socket.io-client').connect(HOST, SETTINGS); 
-	this.translationHandlers = [  /* initialize with a default handler */
-		function (text, translations) {
-			logWithTimestamp(translatorName + " receives "+translations.length+" translations to '"+text + "': "+JSON.stringify(translations));
-		}
-	];
 
 	this.translationSocket.on('connect', function () { 
 		logWithTimestamp(translatorName+" connected to translation server at "+HOST+":"+SETTINGS.port);
 	});
 	
-	var translator = this;
 	this.translationSocket.on('translation', function (result) {
-			for (var i=0; i<translator.translationHandlers.length; ++i) 
-				translator.translationHandlers[i](result.text, result.translations);
+		logWithTimestamp(translatorName + " receives "+result.translations.length+" translations to '"+result.text + "': "+JSON.stringify(result.translations));
 	});
 }
-
 
 exports.Translator.prototype.sendToTranslationServer = function(text, forward) {
 	logWithTimestamp(this.translatorName+" asks: '" + text + "'");
 	this.translationSocket.emit("translate", {
 		text: text,
 		forward: forward,
-		grammar: this.grammar,
-		numOfThreads: /*keep current number of threads*/ 0,
-		entailmentProbabilityThreshold: /* keep current threshold */ 0,
-		entailmentModel: /* keep current model */ undefined
-		}
-		);
+	});
 }
 
 exports.Translator.prototype.onTranslation = function(translationHandler) {
-	this.translationHandlers.push(translationHandler);
+	this.translationSocket.on('translation', function (result) {
+		translationHandler(result.text, result.translations);
+	});
 }
 
 
@@ -64,14 +52,20 @@ if (process.argv[1] === __filename) {
 	logWithTimestamp("translation.js unitest start");
 	var translator1 = new exports.Translator("translator1");
 	var translator2 = new exports.Translator("translator2");
-	translator1.translationSocket.on('connect', function() {
-		translator1.sendToTranslationServer("I offer a salary of 10000 and a car.", true);
-	});
-	translator2.sendToTranslationServer("I offer a salary of 20000 and a car.", true);
-
-	// After several seconds, you should see only 2 results:
-	//   "TranslationServer: 2 translations to 'I offer a salary of 10000...
-	//   "TranslationServer: 2 translations to 'I offer a salary of 20000...
 	
+	translator2.onTranslation(function(text,translations) {
+		console.dir(translations);
+	});
+	
+	translator1.translationSocket.on('connect', function() {
+		translator1.sendToTranslationServer("I agree to offer a wage of 20000 NIS and 10% pension without a car.", true);
+	});
+	translator2.sendToTranslationServer("I agree to your offer.", true);
+
+	// After several seconds, you should see 2 results:
+	//   "translator1 receives 4 translations to 'I offer...
+	//   "translator2 receives 1 translations to 'I agree...
+	//			[ '{"Accept": "previous"}' ]
+
 	logWithTimestamp("translation.js unitest end");
 }
