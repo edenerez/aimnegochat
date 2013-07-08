e94436850c63014a6b5ac1b1104f46c595d29adb
erelsgl <erelsgl@gmail.com>
2013-07-08 13:37:14 +0300

    fix a bug in timer.js

--- timer.js
+++ timer.js
@@ -1,43 +1,42 @@
 // Utilities for creating and displaying a timer
 
 exports.Timer = function(startTimeSeconds, stepSeconds, endTimeSeconds, callback) {
-    if (stepSeconds===0)
-      throw "Cannot start a timer with a zero step";
-    this.startTimeSeconds = startTimeSeconds;
-    this.currentTimeSeconds = startTimeSeconds;
-    
-    // make sure the step is in the right direction, to prevent endless loop:
-    if ((endTimeSeconds>startTimeSeconds) != (stepSeconds>0))
-      this.stepSeconds = -stepSeconds;
+		if (stepSeconds===0)
+			throw new Error("Cannot start a timer with a zero step");
+		this.startTimeSeconds = startTimeSeconds;
+		this.currentTimeSeconds = startTimeSeconds;
+		
+		// make sure the step is in the right direction, to prevent endless loop:
+		if ((endTimeSeconds>startTimeSeconds) != (stepSeconds>0))
+			this.stepSeconds = -stepSeconds;
 
-    var theTimerObject = this;
-    this.interval = setInterval(function() {
-        theTimerObject.currentTimeSeconds += stepSeconds;
-        if (theTimerObject.currentTimeSeconds<0)
-          theTimerObject.currentTimeSeconds = 0;
-        if ((endTimeSeconds>=theTimerObject) != (stepSeconds>0))
-          theTimerObject.stop();
-        callback(theTimerObject.currentTimeSeconds);
-    }, Math.abs(stepSeconds)*1000);
+		var theTimerObject = this;
+		this.interval = setInterval(function() {
+				theTimerObject.currentTimeSeconds += stepSeconds;
+				if ((stepSeconds>0 && theTimerObject.currentTimeSeconds>endTimeSeconds) ||
+				   (stepSeconds<0 && theTimerObject.currentTimeSeconds<endTimeSeconds) )
+						theTimerObject.stop();
+				callback(theTimerObject.currentTimeSeconds);
+		}, Math.abs(stepSeconds)*1000);
 }
 
 exports.Timer.prototype.remainingTimeSeconds = function() {
-  return this.currentTimeSeconds;
+	return this.currentTimeSeconds;
 }
 
 exports.Timer.prototype.timeFromStartSeconds = function() {
-  return this.startTimeSeconds - this.currentTimeSeconds;
+	return this.startTimeSeconds - this.currentTimeSeconds;
 }
 
 exports.Timer.prototype.isRunning = function() {
-  return (this.interval!==null);
+	return (this.interval!==null);
 };
 
 exports.Timer.prototype.stop = function() {
-  if (this.interval) {
-    clearInterval(this.interval);
-    this.interval=null;
-  }
+	if (this.interval) {
+		clearInterval(this.interval);
+		this.interval=null;
+	}
 };
 
 
