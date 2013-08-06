42dedbfb6020a742dace3d0eec5aeab84c57fb4c
erelsgl <erelsgl@gmail.com>
2013-08-06 14:35:30 +0300

    new translation - using web-service

--- translation.js
+++ translation.js
@@ -4,115 +4,44 @@
 
 
 var HOST=process.env.TRANSLATION_SERVER_HOST || "http://irsrv2.cs.biu.ac.il";
-var SETTINGS = {
-	port: process.env.TRANSLATION_SERVER_PORT || 9995, 
-	'force new connection': true, 
-	'sync disconnect on unload': true
-};
-var socket_io_client = require('socket.io-client');
+var PORT=process.env.TRANSLATION_SERVER_PORT || 9995;
+var URL=HOST+":"+PORT+"/get";
+var request = require('request');
 
 function logWithTimestamp(message) {
 	console.log(new Date().toISOString()+" "+message);
 }
 
-function newTranslationSocket(translatorName) {
-	logWithTimestamp(translatorName+" tries to connect to translation server at "+HOST+":"+SETTINGS.port);
-	var translationSocket = socket_io_client.connect(HOST, SETTINGS); 
-
-	translationSocket.on('connect', function () { 
-		logWithTimestamp(translatorName+" connected to translation server at "+HOST+":"+SETTINGS.port);
-	});
-	
-	translationSocket.on('translation', function (result) {
-		logWithTimestamp(translatorName + " receives "+result.translations.length+" translations from '"+result.classifierName+"' to "+JSON.stringify(result.text) + ": "+JSON.stringify(result.translations));
-	});
-
-	translationSocket.on('disconnect', function () { 
-		logWithTimestamp(translatorName+" disconnected from translation server at "+HOST+":"+SETTINGS.port);
-	});
-
-	translationSocket.on('error', function () { 
-		logWithTimestamp(translatorName+" FAILED to connect translation server at "+HOST+":"+SETTINGS.port);
-	});
-
-	translationSocket.on('reconnect_failed', function () { 
-		logWithTimestamp(translatorName+" FAILED to reconnect translation server at "+HOST+":"+SETTINGS.port);
-	});
-
-	return translationSocket;
-}
-
 exports.Translator = function(translatorName) {
 	this.translatorName = translatorName;
-	this.translationSocket = newTranslationSocket(translatorName);
 }
-
 
 /** 
  * Ask the server to translate a certain text.
- * @param callback [optional] - called when the translation arrives.
+ * @param callback [mandatory] - called when the translation arrives.
  */
 exports.Translator.prototype.sendToTranslationServer = function(classifierName, text, forward, callback) {
-	if (!this.translationSocket.socket.connected && !this.translationSocket.socket.connecting && !this.translationSocket.socket.reconnecting) {
-		logWithTimestamp(this.translatorName+" tries to re-connect to translation server at "+HOST+":"+SETTINGS.port);
-		this.translationSocket.socket.reconnect();
-	}
-	logWithTimestamp(this.translatorName+(this.translationSocket.socket.connected? "": " (UNCONNECTED) ")+" asks '"+classifierName+"' to "+(forward? "translate ": "generate ")+ JSON.stringify(text));
+	logWithTimestamp(this.translatorName+" asks '"+classifierName+"' to "+(forward? "translate ": "generate ")+ JSON.stringify(text));
 	var multiple = !(text instanceof Array);
 	
-	if (callback) {
-		var translationSocket=this.translationSocket;
-		var oneTimeCallback = function (result) {
-			var sameClassifier=(result.classifierName===classifierName);
-			var sameText=arraysEqual(result.text,text);
-			var sameForward=(result.forward==forward);
-			console.log("\toneTimeCallback("+sameClassifier+","+sameText+","+sameForward+")");
-			if (sameClassifier && sameText && sameForward) {
+	var url = URL+"?"+
+		"classifierName="+encodeURIComponent(classifierName)+"&"+
+		"text="+encodeURIComponent(JSON.stringify(text))+"&"+
+		(forward? "forward=1&": "")+
+		(multiple? "multiple=1&": "");
+	var self=this;
+	request(url, function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			//console.dir(body);
+			var result = JSON.parse(body);
+			logWithTimestamp(self.translatorName + " receives "+result.translations.length+" translations from '"+result.classifierName+"' to "+JSON.stringify(result.text) + ": "+JSON.stringify(result.translations));
+			if (callback)
 				callback(text, result.translations, forward);
-				translationSocket.removeListener('translation', oneTimeCallback);
-			} else {
-				if (!result.forward && !forward) {
-				//	console.dir(text);
-				//	console.dir(result);
-				}
-			}
-		};
-		this.translationSocket.on('translation', oneTimeCallback);
-	}
-
-	this.translationSocket.emit("translate", {
-		classifierName: classifierName,
-		text: text,
-		forward: forward,
-		multiple: multiple,
+		} else {
+			console.log(url);
+			console.log("error="+error+" response="+JSON.stringify(response));
+		}
 	});
-}
-
-/**
- * Add a permanent listener to translations coming from the server.
- */
-exports.Translator.prototype.onTranslation = function(translationHandler) {
-	this.translationSocket.on('translation', function (result) {
-		translationHandler(result.text, result.translations, result.forward);
-	});
-}
-
-
-/** 
- *http://stackoverflow.com/a/16436975/827927
- */
-function arraysEqual(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length != b.length) return false;
-
-  // If you don't care about the order of the elements inside
-  // the array, you should sort both arrays here.
-
-  for (var i = 0; i < a.length; ++i) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
 }
 
 
@@ -127,22 +56,17 @@ if (process.argv[1] === __filename) {
 	var translator1 = new exports.Translator("translator1");
 	var translator2 = new exports.Translator("translator2");
 	
-	translator2.onTranslation(function(text,translations) {
-		console.dir(translations);
-	});
-	
-	translator1.translationSocket.on('connect', function() {
-		translator1.sendToTranslationServer("Employer", "I agree to offer a wage of 20000 NIS and 10% pension without a car.", true);
-		translator1.sendToTranslationServer("Employer", "{\"Offer\":{\"Pension Fund\":\"10%\"}}", false);
-		translator1.sendToTranslationServer("Employer", ["{\"Offer\":{\"Pension Fund\":\"10%\"}}", "{\"Offer\":{\"Salary\":\"20,000 NIS\"}}"], false);
+	translator1.sendToTranslationServer("Employer", "I agree to offer a wage of 20000 NIS and 10% pension without a car.", true);
+	translator1.sendToTranslationServer("Employer", "{\"Offer\":{\"Pension Fund\":\"10%\"}}", false);
+	translator1.sendToTranslationServer("Employer", ["{\"Offer\":{\"Pension Fund\":\"10%\"}}", "{\"Offer\":{\"Salary\":\"20,000 NIS\"}}"], false);
 
-		translator1.sendToTranslationServer("Candidate", "I want a wage of 20000 NIS and 10% pension with car.", true, function(text, translations, forward) {
-			console.log("Callback called! "+JSON.stringify(translations));
-		});
-		translator1.sendToTranslationServer("Candidate", ["{\"Offer\":{\"Pension Fund\":\"10%\"}}", "{\"Offer\":{\"Salary\":\"20,000 NIS\"}}"], false, function(text, translations, forward) {
-			console.log("Callback called! "+JSON.stringify(translations));
-		});
+	translator1.sendToTranslationServer("Candidate", "I want a wage of 20000 NIS and 10% pension with car.", true, function(text, translations, forward) {
+		console.log("Callback called! "+JSON.stringify(translations));
 	});
+	translator1.sendToTranslationServer("Candidate", ["{\"Offer\":{\"Pension Fund\":\"10%\"}}", "{\"Offer\":{\"Salary\":\"20,000 NIS\"}}"], false, function(text, translations, forward) {
+		console.log("Callback called! "+JSON.stringify(translations));
+	});
+
 	translator2.sendToTranslationServer("Employer", "I agree to your offer.", true);
 	translator2.sendToTranslationServer("Employer", "{\"Accept\":\"previous\"}", false);
 
